3035c76ff61cebaebf09c6c8099752673407bd1f
Alejandro Torres <edialeto@hotmail.com>
2024-05-03 19:39:09 -0500

    pokeneas base project

--- src/models/pokedex.js
+++ src/models/pokedex.js
@@ -4,18 +4,15 @@ const pokeneas = [
         nombre: "Charmander",
         altura: "0.6 m",
         habilidad: "Llamarada",
-        imagen: "charmander.jpg", // Nombre del archivo de imagen en el Cloud Storage Bucket
-        fraseFilosofica: "La llama que arde en su extremo de la cola indica su estado emocional."
     },
     {
         id: 2,
         nombre: "Bulbasaur",
         altura: "0.7 m",
         habilidad: "Espesura",
-        imagen: "bulbasaur.jpg",
-        fraseFilosofica: "A menudo planta semillas en su lomo para hacer bosques."
     },
     
 ];
 
 export default pokeneas;
+
