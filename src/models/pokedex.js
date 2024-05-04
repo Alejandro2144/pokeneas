d1f60e48f58fb5df94f0e8f5599dfaa9abe99be1
Katherine <kbenjumeao@eafit.edu.co>
2024-05-04 10:04:09 -0500

    arreglo pokeneas completo

--- src/models/pokedex.js
+++ src/models/pokedex.js
@@ -1,23 +1,64 @@
 const pokeneas = [
     {
         id: 1,
-        nombre: "Charmander",
-        altura: "0.6 m",
-        habilidad: "Llamarada",
+        nombre: "Alexander (El Flaco)",
+        altura: "1.64 cm",
+        habilidad: "Negociación y Mediación",
     },
     {
         id: 2,
-        nombre: "Bulbasaur",
-        altura: "0.7 m",
-        habilidad: "Espesura",
+        nombre: "Diego (El Paisa)",
+        altura: "1.70 cm",
+        habilidad: "Conocimiento de la Calle",
     },
     {
         id: 3,
-        nombre: "Bulbasaur",
-        altura: "0.7 m",
-        habilidad: "Espesura",
+        nombre: "Kevin (El Mono)",
+        altura: "1.75 cm",
+        habilidad: "Conocimiento de Zonas de Riesgo",
     },
-    
+    {
+        id: 4,
+        nombre: "Yulieth (La China)",
+        altura: "1.53 cm",
+        habilidad: "Manejo de Situaciones de Conflicto en Ambientes Nocturnos",
+    },
+    {
+        id: 5,
+        nombre: "Yesica (La Monita)",
+        altura: "1.61 cm",
+        habilidad: "Negociación de Transacciones de Drogas",
+    },
+    {
+        id: 6,
+        nombre: "Yuliana (La flaquita)",
+        altura: "1.58 cm",
+        habilidad: "Identificación de Calidad y Autenticidad de Sustancias",
+    },
+    {
+        id: 7,
+        nombre: "Maritza (La Maruca)",
+        altura: "1.62 cm",
+        habilidad: "Evasión de las autoridades",
+    },
+    {
+        id: 8,
+        nombre: "Mayerly (La negra)",
+        altura: "1.68 cm",
+        habilidad: "Gestión de Relaciones con Proveedores",
+    },
+    {
+        id: 9,
+        nombre: "Jovani (El Jincho)",
+        altura: "1.79 cm",
+        habilidad: "Conocimiento de Lugares de Venta y Consumo de Drogas:",
+    },
+    {
+        id: 10,
+        nombre: "Freddy (El Pachanga)",
+        altura: "1.69 cm",
+        habilidad: "Conexiones en el Mundo de la Rumba",
+    }
 ];
 
 export default pokeneas;
